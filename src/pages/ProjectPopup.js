8eab1fe9fc42df59cb267defa503c53dc39ceb86
n3wzd <87504447+n3wzd@users.noreply.github.com>
2024-10-08 22:13:42 +0900

    fix: popup scroll

--- src/pages/ProjectPopup.js
+++ src/pages/ProjectPopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ButtonLink from  "../components/ButtonLink.js";
 import ImageSlideMenu from  "../components/ImageSlideMenu.js";
 import ProjectReadme from  "../components/ProjectReadme.js";
@@ -27,11 +27,45 @@ const ProjectPopupPage2 = ({ projectData }) => {
 
 const ProjectPopup = ({ projectData, closePopup }) => {
     const [activeTab, setActiveTab] = useState('page1');
+    const overlayRef = useRef(null);
+    const popupRef = useRef(null);
+
+    useEffect(() => {
+        const overlay = overlayRef.current;
+        const popup = popupRef.current;
+    
+        const handleOuterWheel = (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+        };
+        const handleInnerWheel = (event) => {
+            event.stopPropagation();
+            if(activeTab === 'page1') {
+                event.preventDefault();
+            }
+        };
+    
+        if (overlay) {
+            overlay.addEventListener('wheel', handleOuterWheel, { passive: false });
+        }
+        if (popup) {
+            popup.addEventListener('wheel', handleInnerWheel, { passive: false });
+        }
+    
+        return () => {
+          if (overlay) {
+            overlay.removeEventListener('wheel', handleOuterWheel);
+          }
+          if (popup) {
+            popup.removeEventListener('wheel', handleInnerWheel);
+          }
+        };
+      }, [activeTab]);
 
     return (
         <>
-            <div className="popup-overlay" onClick={closePopup}></div>
-            <div className="popup">
+            <div className="popup-overlay" onClick={closePopup} ref={overlayRef}></div>
+            <div className="popup" ref={popupRef}>
                 <span className="close-btn" onClick={closePopup}>×</span>
                 <div className="tabs">
                     <button className={`tab ${activeTab === 'page1' ? 'active' : ''}`} onClick={() => setActiveTab('page1')}>INTRO</button>
