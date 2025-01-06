0e8fcbeb6d46963d49b0f9c0f5984ec6a45924f2
n3wzd <87504447+n3wzd@users.noreply.github.com>
2025-01-07 05:32:04 +0900

    modify hero title

--- src/pages/HeroSection.js
+++ src/pages/HeroSection.js
@@ -11,7 +11,7 @@ const HeroSection = () => {
       </div>
       <div className="hero-container-item">
         <div className="hero-content">
-          <h1>Hello, I'm Testman</h1>
+          <h1>Crafting Solutions, One Project at a Time</h1>
           <p>기술로 세상을 연결하는 개발자로서, 혁신과 문제 해결을 통해 가치를 창출하며 속도와 역량으로 목표를 향해 나아갑니다.</p>
           <ButtonLink text="View My Projects" href="#about-section" openNewTab="false"/>
         </div>
